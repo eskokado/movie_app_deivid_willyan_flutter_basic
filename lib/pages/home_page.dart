e4ed40caf92063479797a900b66d8ed33cf4ffa6
eskokado <eskokado@gmail.com>
2022-06-23 06:42:43 -0300

    feat: home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:movieapp/controllers/movie_controller.dart';
+import 'package:movieapp/models/movies_model.dart';
+import 'package:movieapp/repositories/movies_repository_impl.dart';
+import 'package:movieapp/services/dio_service_impl.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({ Key? key }) : super(key: key);
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final MovieController _controller = MovieController(
+    MoviesRepositoryImpl(
+      DioServiceImpl(),
+    ),
+  );
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      body: ValueListenableBuilder<Movies?>(
+        valueListenable: _controller.movies,
+        builder: (_, movies, __) {
+          return movies != null
+              ? ListView.builder(
+                  itemCount: movies.listMovies.length,
+                  itemBuilder: (_, index) =>
+                      Text(movies.listMovies[index].title),
+                )
+              : Container();
+        },
+      ),
     );
   }
 }
